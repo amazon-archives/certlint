c475693ca0d7449295ae229ee0b857699d446b87
Peter Bowen <pzb@amazon.com>
2016-08-15 22:21:59 -0700

    Refactor handling of generalNames

--- lib/certlint/generalnames.rb
+++ lib/certlint/generalnames.rb
@@ -16,111 +16,190 @@
 module CertLint
   class GeneralNames
     # Allow RFC defying '*' and '_'
-    DLABEL = '((?!-)[A-Za-z0-9*_-]{1,63}(?<!-))'
+    DLABEL = '((?!-)[A-Za-z0-9*_-]{1,63}(?<!-))'.freeze
     FQDN = /\A(#{DLABEL}\.)*#{DLABEL}\z/
 
     # Email addresses don't have '*' or '_'
     EMAIL_SPLITTER = /\A(.*)@([^@]*)\z/
-    EMAIL_DLABEL = '((?!-)[A-Za-z0-9-]{1,63}(?<!-))'
+    EMAIL_DLABEL = '((?!-)[A-Za-z0-9-]{1,63}(?<!-))'.freeze
     EMAIL_DOMAIN = /\A(#{EMAIL_DLABEL}\.)*#{EMAIL_DLABEL}\z/
-    EMAIL_ATOM = "[A-Za-z0-9!#\$%&'*+/=?^_`{|}~-]+"
+    EMAIL_ATOM = "[A-Za-z0-9!#\$%&'*+/=?^_`{|}~-]+".freeze
     EMAIL_LOCAL_PART = /\A(#{EMAIL_ATOM})(\.#{EMAIL_ATOM})*\z/
+
+    OTHERNAMES = {
+      '1.2.410.200004.10.1.1' => nil,
+      '1.3.6.1.2.1.32' => nil,
+      '1.3.6.1.4.1.11801.2.1' => nil,
+      '1.3.6.1.4.1.311.20.2.3' => nil,
+      '1.3.6.1.4.1.311.25.1' => nil,
+      '1.3.6.1.4.1.8321.1' => nil,
+      '1.3.6.1.5.5.7' => nil,
+      '1.3.6.1.5.5.7.8.5' => nil,
+      '1.3.6.1.5.5.7.8.7' => nil,
+      '2.16.76.1.3.2' => nil,
+      '2.16.76.1.3.3' => nil,
+      '2.16.76.1.3.4' => nil,
+      '2.16.76.1.3.7' => nil,
+      '2.16.76.1.3.8' => nil,
+      '2.16.862.2.1' => nil,
+      '2.16.862.2.2' => nil,
+      '2.16.862.2.3' => nil,
+      '2.16.862.2.4' => nil,
+      '2.16.862.2.5' => nil,
+      '2.16.862.2.6' => nil,
+      '2.5.4.13' => nil,
+      '2.5.5.5' => nil
+    }
+
+    def self.othername(value, is_constraint = false)
+      messages = []
+
+      # Sequence of oid, value
+      oid = value.first.oid
+      if OTHERNAMES.key?(oid)
+        checker = OTHERNAMES[oid]
+        case checker
+        when nil
+          messages << "I: No checks for OtherName type #{oid}"
+        else
+          messages << "I: Missing check for OtherName #{checker}"
+        end
+      else
+        messages << "I: No checks for unknown OtherName type #{oid}"
+      end
+      messages
+    end
+
+    def self.rfc822name(orig_addr, is_constraint = false)
+      messages = []
+      if orig_addr.nil? || orig_addr.empty?
+        messages << 'E: RFC822Name has empty value'
+        return messages # Fatal to this entry
+      end
+      if orig_addr.include? "\0"
+        messages << 'E: RFC822Name includes null'
+      end
+      addr = orig_addr.strip.chomp('.')
+      if orig_addr != addr
+        messages << 'E: Invalid padding in RFC822Name'
+      end
+      if addr.include? '@'
+        p = EMAIL_SPLITTER.match(addr)
+        local_part = p[1]
+        domain_part = p[2]
+      else
+        if !is_constraint
+          messages << 'E: RFC822Name without @'
+        end
+        local_part = nil
+        domain_part = addr
+      end
+
+      if domain_part.start_with?('.')
+        if !is_constraint
+          messages << 'E: RFC822Name domain must not start with .'
+        end
+        domain_part = domain_part[1..-1]
+      end
+
+      if domain_part.empty?
+        messages << 'E: RFC822Name without domain'
+      elsif EMAIL_DOMAIN !~ domain_part
+        messages << 'E: RFC822Name with invalid domain'
+      end
+
+      # Check for IDNs; https://tools.ietf.org/html/rfc5891#section-5.4
+      domain_part.split('.').each do |label|
+        next unless label.start_with? 'xn--'
+        ulabel = SimpleIDN.to_unicode(label)
+        if ulabel.respond_to? :unicode_normalize
+          ulabel_nfc = ulabel.unicode_normalize(:nfc)
+        else
+          ulabel_nfc = ulabel.to_nfc
+        end
+        if ulabel != ulabel_nfc
+          messages << 'E: Internationalized domain names must be in unicode normalization form C'
+        end
+      end
+
+      # If the local part is nil (e.g. name constraint)
+      # don't do checks on it
+      if local_part.nil?
+        return messages
+      end
+
+      if local_part.empty?
+        # Empty can happen if the addr is "@example.com"
+        messages << 'E: RFC822Name without local part'
+      elsif local_part.include? '"'
+        messages << 'W: RFC822Name with quoted local part'
+      elsif EMAIL_LOCAL_PART !~ local_part
+        messages << 'E: RFC822Name with invalid local part'
+      end
+
+      messages
+    end
+
+    def self.dnsname(orig_fqdn, is_constraint = false)
+      messages = []
+      if orig_fqdn.nil? || orig_fqdn.empty?
+        messages << 'E: DNSName is empty'
+        return messages # Fatal to this entry
+      end
+      if orig_fqdn.include? "\0"
+        messages << 'E: DNSName includes null'
+      end
+      fqdn = orig_fqdn.strip.chomp('.')
+      if orig_fqdn != fqdn
+        messages << 'E: DNSName is not in preferred syntax'
+      end
+      # Name Constraints can start with '.'
+      if fqdn.start_with?('.')
+        if !is_constraint
+          messages << 'E: DNSName must not start with .'
+        end
+        fqdn = fqdn[1..-1]
+      end
+      unless FQDN.match(fqdn)
+        messages << 'E: DNSName is not FQDN'
+      end
+      if fqdn.length > 253
+        messages << 'E: FQDN in DNSName is too long'
+      end
+      if is_constraint
+        if fqdn.include?('*')
+          messages << 'E: Wildcard in FQDN'
+        end
+      end
+      if fqdn.include?('_')
+        messages << 'W: Underscore should not appear in DNS names'
+      end
+      # Check for IDNs; https://tools.ietf.org/html/rfc5891#section-5.4
+      fqdn.split('.').each do |label|
+        next unless label.start_with? 'xn--'
+        ulabel = SimpleIDN.to_unicode(label)
+        if ulabel.respond_to? :unicode_normalize
+          ulabel_nfc = ulabel.unicode_normalize(:nfc)
+        else
+          ulabel_nfc = ulabel.to_nfc
+        end
+        if ulabel != ulabel_nfc
+          messages << 'E: Internationalized domain names must be in unicode normalization form C'
+        end
+      end
+      messages
+    end
 
     # set is_san to true for SubjectAltName entries, false for Name Contstraints
     def self.lint(genname, is_san = true)
       messages = []
       case genname.tag
       when 0 # OtherName
-        # Sequence of oid, value
-        oid = genname.value.first.oid
-        messages << "I: No checks for OtherName type #{oid}"
-        # No checks
+        messages += othername(genname.value, !is_san)
       when 1 # RFC822Name
-        orig_addr = genname.value
-        if orig_addr.nil? || orig_addr.empty?
-          messages << 'E: RFC822Name has empty value'
-          return messages # Fatal to this entry
-        end
-        if orig_addr.include? "\0"
-          messages << 'E: RFC822Name includes null'
-        end
-        addr = orig_addr.strip.chomp('.')
-        if orig_addr != addr
-          messages << 'E: Invalid padding in RFC822Name'
-        end
-        if addr.include? '@'
-          p = EMAIL_SPLITTER.match(addr)
-          local_part = p[1]
-          domain_part = p[2]
-        else
-          if is_san
-            messages << 'E: RFC822Name without @'
-          end
-          local_part = nil
-          domain_part = addr
-        end
-
-        if domain_part.start_with?('.')
-          if is_san
-            messages << 'E: RFC822Name domain must not start with .'
-          end
-          domain_part = domain_part[1..-1]
-        end
-
-        if domain_part.empty?
-          messages << 'E: RFC822Name without domain'
-        elsif EMAIL_DOMAIN !~ domain_part
-          messages << 'E: RFC822Name with invalid domain'
-        end
-
-        # If the local part is nil (e.g. name constraint)
-        # don't do checks on it
-        if local_part.nil?
-          return messages
-        end
-
-        if local_part.empty?
-          # Empty can happen if the addr is "@example.com"
-          messages << 'E: RFC822Name without local part'
-        elsif local_part.include? '"'
-          messages << 'W: RFC822Name with quoted local part'
-        elsif EMAIL_LOCAL_PART !~ local_part
-          messages << 'E: RFC822Name with invalid local part'
-        end
+        messages += rfc822name(genname.value, !is_san)
       when 2 # DNSName
-        orig_fqdn = genname.value
-        if orig_fqdn.nil? || orig_fqdn.empty?
-          messages << 'E: DNSName is empty'
-          return messages # Fatal to this entry
-        end
-        if orig_fqdn.include? "\0"
-          messages << 'E: DNSName includes null'
-        end
-        fqdn = orig_fqdn.strip.chomp('.')
-        if orig_fqdn != fqdn
-          messages << 'E: DNSName is not in preferred syntax'
-        end
-        # Name Constraints can start with '.'
-        if fqdn.start_with?('.')
-          if is_san
-            messages << 'E: DNSName must not start with .'
-          end
-          fqdn = fqdn[1..-1]
-        end
-        unless FQDN.match(fqdn)
-          messages << 'E: DNSName is not FQDN'
-        end
-        if fqdn.length > 253
-          messages << 'E: FQDN in DNSName is too long'
-        end
-        unless is_san
-          if fqdn.include?('*')
-            messages << 'E: Wildcard in FQDN'
-          end
-        end
-        if fqdn.include?('_')
-          messages << 'W: Underscore should not appear in DNS names'
-        end
+        messages += dnsname(genname.value, !is_san)
       when 3 # X400Address
         orig = genname.value
         if orig.nil? || orig.empty?
