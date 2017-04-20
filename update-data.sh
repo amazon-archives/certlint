e14416b0a7d1f94835ed27d3eaae8e9630bc1583
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2017-04-20 18:55:42 -0400

    request all data urls via HTTPS.

--- update-data.sh
+++ update-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o data/newgtlds.csv https://newgtlds.icann.org/newgtlds.csv
-curl -o data/root.zone http://www.internic.net/domain/root.zone
-curl -o data/special-use-domain.csv http://www.iana.org/assignments/special-use-domain-names/special-use-domain.csv
+curl -o data/root.zone https://www.internic.net/domain/root.zone
+curl -o data/special-use-domain.csv https://www.iana.org/assignments/special-use-domain-names/special-use-domain.csv
 curl -o data/public_suffix_list.dat https://publicsuffix.org/list/public_suffix_list.dat
-curl -o data/ldap-names.csv http://www.iana.org/assignments/ldap-parameters/ldap-parameters-3.csv
+curl -o data/ldap-names.csv https://www.iana.org/assignments/ldap-parameters/ldap-parameters-3.csv
