bbd981ffed0de1b2582b757b6a0638486b1b6a69
Matt Palmer <mpalmer@hezmatt.org>
2017-06-19 14:28:53 +1000

    Check for SHA1 collision attempts

--- ext/certlint_ext.c
+++ ext/certlint_ext.c
@@ -1,6 +1,9 @@
 /*
- * Ruby bindings for interfacing with asn1c-generated ASN.1 PDU parsing code.
- * Copyright (c) 2016 Matt Palmer <matt@hezmatt.org>
+ * Ruby bindings for a variety of C libraries used for various parts of
+ * certlint's work.  See README in this directory for build instructions and
+ * references to the library code used.
+ *
+ * Copyright (c) 2016,2017 Matt Palmer <matt@hezmatt.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not
  * use this file except in compliance with the License. A copy of the License
@@ -21,6 +24,7 @@
 #include <errno.h>
 
 #include <asn_application.h>
+#include "sha1collisiondetection/lib/sha1.h"
 
 extern asn_TYPE_descriptor_t *asn_pdu_collection[];
 
@@ -36,7 +40,7 @@ static asn_TYPE_descriptor_t *asn1pdu_type_descriptor(VALUE pdu_type) {
 		}
 		pdu++;
 	}
-	
+
 	rb_raise(rb_eArgError, "Unknown PDU type");
 	return NULL;  /* Unreachable, we hope */
 }
@@ -110,13 +114,13 @@ static VALUE asn1pdu_to_der(VALUE self) {
 	VALUE t = rb_iv_get(self, "@pdu_type");
 	void *pdu_structure = asn1pdu_decode_pdu(d, t);
 	asn_enc_rval_t rv;
-	
+
 	rv.encoded = -1;
 
 	while (rv.encoded == -1) {
 		if (derbuf) free(derbuf);
 		derlen *= 2;
-		
+
 		derbuf = malloc(derlen);
 		if (derbuf == NULL) {
 			rb_raise(rb_eNoMemError, "Unable to allocate memory for derbuf");
@@ -127,15 +131,35 @@ static VALUE asn1pdu_to_der(VALUE self) {
 	}
 
 	ASN_STRUCT_FREE(*asn1pdu_type_descriptor(t), pdu_structure);
-	
+
 	return rb_str_new(derbuf, rv.encoded);
 }
 
-void Init_asn1validator() {
+static VALUE sha1collision_check(VALUE self, VALUE data) {
+	SHA1_CTX ctx;
+	unsigned char hash[20];
+	SHA1DCInit(&ctx);
+	int iscoll;
+
+	SHA1DCSetSafeHash(&ctx, 0);
+	SHA1DCSetUseUBC(&ctx, 0);
+	SHA1DCUpdate(&ctx, RSTRING_PTR(data), RSTRING_LEN(data));
+	iscoll = SHA1DCFinal(hash,&ctx);
+
+	if (iscoll) {
+		return Qtrue;
+	} else {
+		return Qfalse;
+	}
+}
+
+void Init_certlint_ext() {
 	mod_certlint = rb_define_module("CertLint");
 	class_certlint_asn1pdu = rb_define_class_under(mod_certlint, "ASN1Validator", rb_cObject);
 
 	rb_define_method(class_certlint_asn1pdu, "initialize", asn1pdu_initialize, 2);
 	rb_define_method(class_certlint_asn1pdu, "check_constraints", asn1pdu_check_constraints, 0);
 	rb_define_method(class_certlint_asn1pdu, "to_der", asn1pdu_to_der, 0);
+
+	rb_define_singleton_method(mod_certlint, "sha1_collision?", sha1collision_check, 1);
 }
